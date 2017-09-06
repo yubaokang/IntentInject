04bceff1ce7cbf3020ec390f569dcf03bed6918b
ybk <ybk@ybk.local>
2017-09-06 21:46:37 +0800

    submit 1.3

--- app/src/main/java/com/ybk/intent/inject/BaseActivity.java
+++ app/src/main/java/com/ybk/intent/inject/BaseActivity.java
@@ -3,8 +3,6 @@ package com.ybk.intent.inject;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.ybk.intent.inject.api.IntentInject;
-
 public class BaseActivity extends AppCompatActivity {
 
     @Override
